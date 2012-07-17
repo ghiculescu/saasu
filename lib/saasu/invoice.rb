c2b672b46b76e2d094ef5e5f32a159404773738b
chriskruger <chris.kruger@agworld.com.au>
2012-07-17 10:26:33 +0800

    fleshed out invoices for find queries

--- lib/saasu/invoice.rb
+++ lib/saasu/invoice.rb
@@ -3,16 +3,29 @@ module Saasu
   class Invoice < Transaction
 
     elements "transactionType" => :string,
+            "invoiceType" => :string,
+            "date" => :string,
             "contactUid" => :string,
             "shipToContactUid" => :integer,
+            "folderUid" => :integer,
+            "tags" => :string,
+            "reference" => :string,
+            "summary" => :string,
+            "ccy" => :string,
+            "autoPopulateFxRate" => :boolean,
+            "fcToBxFxRate" => :decimal,
+            "notes" => :string,
             "externalNotes" => :string,
+            "requiresFollowUp" => :boolean,
             "dueOrExpiryDate" => :date,
+            "tradingTerms" => :string,
             "layout" => :string,
             "status" => :string,
             "invoiceNumber" => :string,
             "purchaseOrderNumber" => :string,
             "invoiceItems" => :array,
             "quickPayment" => :array,
+            "payments" => :array,
             "isSent" => :boolean
   end
   
